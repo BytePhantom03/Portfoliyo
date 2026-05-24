3b04573f8756173e809636564758990651e9b3c9
Ravi Zapadiya <167902767+BytePhantom03@users.noreply.github.com>
2026-05-24 16:01:36 +0530

    update contactsData

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'mrravizapadiya03@gmail.com',
     phone: '+91 9574982254',
-    address: 'Navrangpure, Ahemedabad - 380015 ',
+    address: 'Navrangpura, Ahemedabad - 380015 ',
     github: "https://github.com/BytePhantom03",
     facebook: "https://www.facebook.com",
     linkedIn: "https://www.linkedin.com/in/zapadiya-ravi-205a69273/",
