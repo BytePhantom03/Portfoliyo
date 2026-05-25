978a9f029c9ff6720f21e9e75214f616536ffe65
BytePhantom03 <ravikumarzapadiya@gmail.com>
2026-05-25 17:32:12 +0530

    Update Personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,8 +5,8 @@ export const personalData = {
   description:
     "My name is Ravi Zapadiya. I am a passionate and enthusiastic Computer Engineering student with a strong interest in Artificial Intelligence, Machine Learning, Deep Learning, NLP, and Full-Stack Development. I am a quick learner with a self-learning mindset and strong problem-solving skills, and I enjoy building real-world projects that solve practical problems. I have hands-on experience with technologies like Python, React.js, Flask, Machine Learning, Deep Learning, RAG pipelines, and Large Language Models through projects, internships, and hackathons.",
   email: "mrravizapadiya03@gmail.com",
-  phone: "+91 9571982254",
-  address: "Navrangpure, Ahemedabad - 380015 ",
+  phone: "+91 9574982254",
+  address: "Navrangpura, Ahemedabad - 380015 ",
   github: "https://github.com/BytePhantom03",
   facebook: "https://www.facebook.com",
   linkedIn: "https://www.linkedin.com/in/zapadiya-ravi-205a69273/",
